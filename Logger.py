f8e49315b718d3a560f56bf521a8e413abedd121
007Jagaban <ovuede.mudiaga@gmail.com>
2021-08-18 04:44:51 +0100

    Refined code

--- Logger.py
+++ Logger.py
@@ -10,6 +10,7 @@ class Logger():
         self.axes = [] #to store the number of plot axis that will be print out
         self.fig, self.ax = plt.subplots(figsize=(15, 10))
         #self.ax = []
+        self.dict = {}
 
     def get_data(self):
         base_log = lasio.read('/Users/mudi/OneDrive/Codes/RE_WorkFold/Elcrest_Gbetiokun-6_LWD_Data \
@@ -17,41 +18,35 @@ class Logger():
         self.base_log = base_log #call is self.base_log so it can be manipulated
         self.base_log_frame = self.base_log.df() #create dataframe
 
-    def plot_axis(self,count = 3):
+    def plot_axis(self,count = 1): #Plot 1 chart
         self.count = count
         list = [self.count]
-        #axes = []
         while (self.count) > 0:
             self.axes.append('ax{}'.format(list[-1]))
             list.append(self.count-1)
             self.count = self.count -1
         print(self.axes)
-        #self.fig.savefig('test1')
         # Set up the plot axes
         for self.ax in self.axes:
             for self.ax_no in range(len(self.axes)):
-                self.ax = (plt.subplot2grid((1, 4), (0, self.ax_no), rowspan=1, colspan=1)).plot(self.base_log_frame["GR"], self.base_log_frame.index, color="green", linewidth=0.5)
-                self.ax = (plt.subplot2grid((1, 4), (0, self.ax_no), rowspan=1, colspan=1)).set_xlabel("Gamma: GB_6")
-                self.ax = (plt.subplot2grid((1, 4), (0, self.ax_no), rowspan=1, colspan=1)).xaxis.label.set_color("green")
-                self.ax = (plt.subplot2grid((1, 4), (0, self.ax_no), rowspan=1, colspan=1)).set_xlim(0, 200)
-                self.ax = (plt.subplot2grid((1, 4), (0, self.ax_no), rowspan=1, colspan=1)).set_ylabel("Depth (ft)")
-                self.ax = (plt.subplot2grid((1, 4), (0, self.ax_no), rowspan=1, colspan=1)).tick_params(axis='x', colors="green")
-                self.ax = (plt.subplot2grid((1, 4), (0, self.ax_no), rowspan=1, colspan=1)).spines["top"].set_edgecolor("green")
-                self.ax = (plt.subplot2grid((1, 4), (0, self.ax_no), rowspan=1, colspan=1)).title.set_color('green')
-                self.ax = (plt.subplot2grid((1, 4), (0, self.ax_no), rowspan=1, colspan=1)).set_xticks([0, 50, 100, 150, 200])
-        print(self.ax)
-        self.fig.savefig('test1')
+                self.dict[self.ax] = (plt.subplot2grid((1, 4), (0, self.ax_no), rowspan=1, colspan=1))
+        print(self.dict.items(), self.dict.values(), self.dict.keys())
 
     def plot_gr(self):
-        self.get_axis()
-        # Gamma Ray track
-        #for self.ax in self.axes:
-        self.ax.set_ylim(9950, 9500)
-        self.ax.grid(which='major', color='lightgrey', linestyle='-')
-        self.ax.xaxis.set_ticks_position("top")
-        self.ax.xaxis.set_label_position("top")
+        self.plot_axis(count=1)
+        for ax in self.dict.values():
+            ax.plot(self.base_log_frame["GR"], self.base_log_frame.index, color="green", linewidth=0.5)
+            ax.set_xlabel("Gamma: GB_6")
+            ax.xaxis.label.set_color("green")
+            ax.set_xlim(0, 200)
+            ax.set_ylabel("Depth (ft)")
+            ax.tick_params(axis='x', colors="green")
+            ax.spines["top"].set_edgecolor("green")
+            ax.title.set_color('green')
+            #ax.set_xticks([0, 50, 100, 150, 200])
         self.fig.savefig('test1')
 
-test = Logger('Gbet_6')
 
-print(test.plot_axis(count=3))
+test = Logger('Gbet_6')
+#print(test.plot_axis(count=1))
+print(test.plot_gr())
